843515d736ebe8c5d384f2e57ef89c5322651183
Masayuki <soy2.san@gmail.com>
2020-09-21 00:43:28 +0900

    delete extra & make controller

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,26 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    logger('welcome route.');
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     logger('welcome route.');
+//     return view('welcome');
+// });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'ReviewController@index')->name('index');
