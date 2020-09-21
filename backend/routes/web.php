b7950e679bd622e0b2d058c56902c500e4acd206
Masayuki <soy2.san@gmail.com>
2020-09-21 16:25:12 +0900

    review page & routing

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,13 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-// Route::get('/', function () {
-//     logger('welcome route.');
-//     return view('welcome');
-// });
+Route::get('/', 'ReviewController@index')->name('index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/review', 'ReviewController@create')->name('create');
 
-Route::get('/', 'ReviewController@index')->name('index');
+Route::get('/home', 'HomeController@index')->name('home');
