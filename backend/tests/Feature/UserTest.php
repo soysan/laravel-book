9d8d518804237776e1917b98642894e4e91c97a1
Masayuki <soy2.san@gmail.com>
2020-10-18 18:54:17 +0900

    add some user cases

--- backend/tests/Feature/UserTest.php
+++ backend/tests/Feature/UserTest.php
@@ -7,10 +7,12 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\User;
 use Faker\Factory;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class UserTest extends TestCase
 {
     use RefreshDatabase;
+    use DatabaseMigrations;
     /**
      * db connection check
      *
@@ -18,8 +20,34 @@ class UserTest extends TestCase
      */
     public function testCreateAUser()
     {
+        Factory(User::class)->create([
+            'name' => 'jack',
+            'email' => 'aaa.bbb@gmail.com',
+            'password' => 'ABCDEFGH'
+        ]);
+        Factory(User::class, 10)->create();
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'jack',
+            'email' => 'aaa.bbb@gmail.com',
+            'password' => 'ABCDEFGH'
+        ]);
+    }
+
+
+    public function testCreateAUserLogin()
+    {
+        Factory(User::class)->create([
+            'name' => 'jack',
+            'email' => 'aaa.bbb@gmail.com',
+            'password' => 'ABCDEFGH',
+        ]);
         Factory(User::class)->create();
 
-        $this->assertCount(1, User::all());
+        $this->assertDatabaseHas('users', [
+            'name' => 'jack',
+            'email' => 'aaa.bbb@gmail.com',
+            'password' => 'ABCDEFGH'
+        ]);
     }
 }
