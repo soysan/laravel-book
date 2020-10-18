ecaba425a93799ddc3a25eb37f5c5646d2ec3f84
Masayuki <soy2.san@gmail.com>
2020-10-18 18:53:25 +0900

    clean up

--- backend/tests/Feature/ExampleTest.php
+++ backend/tests/Feature/ExampleTest.php
@@ -15,8 +15,6 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $response = $this->get('/');
-
-        // $response->assertStatus(200);
-        $response->assertStatus(302);
+        $response->assertStatus(200);
     }
 }
